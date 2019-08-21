dcfb8845bbf56a9ab2eab3246bbad9322b589e44
rjp <zimpenfish@gmail.com>
2019-08-21 12:02:08 +0100

    Server updates
    Add a simple `map` database for now
    Updated `PutPort` and `GetPortByShortcode` to query the map

--- cmd/server.go
+++ cmd/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bitbucket.org/rjp/ports/portrpc"
 	"context"
+	"errors"
 	"fmt"
 	grpc "google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -10,19 +11,29 @@ import (
 	"net"
 )
 
+var Database map[string]portrpc.Port
+
 type ourClient struct{}
 
 func (pdbs *ourClient) PutPort(ctx context.Context, port *portrpc.Port) (*portrpc.PutResponse, error) {
 	fmt.Printf("%s\n", port.String())
+	Database[port.Shortcode] = *port
 	return &portrpc.PutResponse{Response: "yes"}, nil
+
 }
 
 func (pdbs *ourClient) GetPortByShortcode(ctx context.Context, shortcode *portrpc.Shortcode) (*portrpc.Port, error) {
-	x := portrpc.Port{}
-	return &x, nil
+	fmt.Printf("HELLO SOMEONE WANTED %s\n", shortcode.Shortcode)
+	if port, ok := Database[shortcode.Shortcode]; ok {
+		return &port, nil
+	} else {
+		return &portrpc.Port{}, errors.New("No port for shortcode " + shortcode.Shortcode)
+	}
 }
 
 func main() {
+	Database = make(map[string]portrpc.Port)
+
 	tcpport := 9387
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", tcpport))
 	if err != nil {
